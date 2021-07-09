67c61fd3e27b2fc8ec75d61cc116243a3eccbee2
Barkah026 <Trisubagjab@gmail.com>
2021-07-09 09:47:04 -0700

    Calculator Tugas

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message : "---Calculator Barkahts---"
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: rgb(0, 0, 0);
+}
+</style>
